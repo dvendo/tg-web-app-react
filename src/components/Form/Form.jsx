42b22c4d46ef485d403199f260bcebb622c96fe7
dvendo <dominikavender5@gmail.com>
2024-07-04 12:15:08 +0300

    initial

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,6 +3,19 @@ import './Form';
 
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
 
     return (
         <div className={'form'}>
@@ -11,13 +24,17 @@ const Form = () => {
             className={'input'} 
             type='text' 
             placeholder={'Страна'}
+            value={country}
+            onChange={onChangeCountry}
             />        
             <input 
             className={'input'} 
             type='text' 
             placeholder={'Улица'}
+            value={street}
+            onChange={onChangeStreet}
             />
-           <select className={'select'}>
+           <select value={subject} onChange={onChangeSubject} className={'select'}>
             <option value={'physical'}>Физ.лицо</option>
             <option value={'legal'}>Юр.лицо</option>
            </select>
